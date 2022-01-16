03e70976833b2d63564ad9912ae03de8999ea624
Michael Bowen <michaelbowen1092@gmail.com>
2022-01-15 22:37:28 -0500

    commit tree b8da2f7a68b548b65d80934e0346aaaf1d9be736

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Code your solution in this file!
+function distanceFromHqInBlocks(hqInBlocks){
+    console.log(hqInBlocks)
+    if (hqInBlocks >= 42){
+        return hqInBlocks - 42
+    }
+    else if(hqInBlocks <= 42){
+        return 42 - hqInBlocks
+    }
+
+    
+}
+function distanceFromHqInFeet(feet){
+    console.log(feet)
+    if (feet >= 42){
+        return (feet - 42)*264
+    }
+    else if (feet <= 42){
+        return (42 - feet)*264
+    }
+
+}
+function distanceTravelledInFeet(to, from){
+    console.log(to, from)
+    if (to <= from){
+        return (from - to)*264
+    }
+    else if (to >= from){
+        return (to - from)*264
+    }
+}
+function calculatesFarePrice(start, destination){
+    if(start, destination = 0){
+        return 0
+    }
+}
